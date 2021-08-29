6cbefacb328153875357a6dcb2003da20eb845e8
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-08-29 11:28:10 -0400

    Add files via upload

---
+++ DoNow4.2-main/README.md
@@ -0,0 +1,3 @@
+# DoNow4.2
+Another school thing.
+# to use go here: https://someaspy.github.io/DoNow4.2/
---
+++ DoNow4.2-main/code.js
@@ -0,0 +1,6 @@
+let str=prompt("Enter A word!");
+str=str.split("");
+str.forEach(char => {
+    alert(char);
+    console.log(char);
+});
