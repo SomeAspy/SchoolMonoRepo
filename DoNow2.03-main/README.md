bd2522b5883f49a0b71698a3aae30b654700fd0b
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-08-29 11:13:04 -0400

    Add files via upload

---
+++ DoNow2.03-main/README.md
@@ -0,0 +1,3 @@
+# DoNow2.03
+what even is this
+[![Run on Repl.it](https://repl.it/badge/github/SomeAspy/DoNow2.03)](https://repl.it/github/SomeAspy/DoNow2.03)
---
+++ DoNow2.03-main/code.py
@@ -0,0 +1,17 @@
+#Aiden B.
+#3/9/2021
+#DoNow2.03
+
+from time import sleep # import sleep from the time library
+
+print("Use 24 hour time because I don't feel like checking AM/PM garbage.\nFormat times as shown:\n 10:00 would be 1000, 12:00 = 1200, etc.\nBased off my real schedule on a Saturday")
+
+garbage=input("Time:\n") #Put useless stuff in the trash
+
+del garbage #Take out the trash 
+
+print("\nIgnoring user input... Please wait...\n") # Notify the user of very important information
+
+sleep(2) #Wait 2 seconds
+
+print("I don't plan things, and neither do I want random people on the internet knowing what I do on a day to day basis.") # What
