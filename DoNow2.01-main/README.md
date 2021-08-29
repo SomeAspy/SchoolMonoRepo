a83160c1bfbc894f126f6abdbd88663ea511ad6b
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-08-29 11:19:18 -0400

    Add files via upload

---
+++ DoNow2.01-main/README.md
@@ -0,0 +1,3 @@
+# DoNow2.01
+[![Run on Repl.it](https://repl.it/badge/github/SomeAspy/DoNow2.01)](https://repl.it/github/SomeAspy/DoNow2.01)
+Data Types and casting I think
---
+++ DoNow2.01-main/code.py
@@ -0,0 +1,23 @@
+# Do now 2.01
+# 3/2/2021
+# Aiden B.
+
+x_stage1 = 123
+y_stage1 = 456
+
+x_stage2 = str(x_stage1)
+y_stage2 = str(y_stage1)
+
+print(x_stage2+y_stage2)
+
+print("\nPart 1:\n") #part one answers
+print("x_stage1: "+str(type(x_stage1)))
+print("y_stage1: "+str(type(y_stage1)))
+
+print("\nPart 2:\n") #part two answers
+print("x_stage2: "+str(type(x_stage2)))
+print("y_stage2: "+str(type(y_stage2)))
+
+print("\nPart 3:\n") #part 3
+print('print(int("100"))')
+print(int("100"))
