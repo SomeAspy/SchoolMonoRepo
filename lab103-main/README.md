79b7d65b58eaf35dd3d36753b82ed9323d9e19f6
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-08-29 11:23:34 -0400

    Add files via upload

---
+++ lab103-main/README.md
@@ -0,0 +1,10 @@
+# lab103
+
+[![Run on Repl.it](https://repl.it/badge/github/SomeAspy/lab103)](https://repl.it/github/SomeAspy/lab103)
+does something I think
+
+part 2
+
+- you can print cats are cool by changing the animals definition to cats
+- wrong syntax produces an error.
+- part 3 is just missing, there was no scratch program linked
---
+++ lab103-main/code.py
@@ -0,0 +1,29 @@
+# Aiden Baker
+# 2/12/2021
+# lab103
+
+print("1")
+print(1)
+print(1+2)
+print("1"+"2")
+print("this"+" "+"is"+" "+"a"+" "+"sentence"+".") # Ok but why?
+#PART2
+print("-----------------------")
+
+animal="dogs"
+print(animal+" are really cool.")
+
+# print(dogs+" are cool.")
+
+#PART3
+print("-----------------------")
+
+print("Zero is ",4/4-4/4)
+print("One is ",44/44)
+print("Two is ",4/4+4/4)
+print("Three is ",(4*4-4)/4)
+print("Four is ",(4/4)**4*4)
+print("Five is ",4+(4/4)**4)
+# ok but why would we ever need this lol
+
+print("a\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\n")
