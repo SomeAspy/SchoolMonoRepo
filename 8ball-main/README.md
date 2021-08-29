9e03e72fca64adc35ba1e81d0a9d9ba454ea8bc2
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-08-29 11:04:36 -0400

    Add files via upload

---
+++ 8ball-main/README.md
@@ -0,0 +1,2 @@
+# 8ball
+a school thing that does eh
---
+++ 8ball-main/code.py
@@ -0,0 +1,13 @@
+# Aiden B.
+# Lab 3.01
+# 3/30/2021
+from random import randint
+x=randint(0,7)
+if x==0:print("Outlook is good")
+elif x==1:print("Ask again later")
+elif x==2:print("Yes")
+elif x==3:print("No")
+elif x==4:print("Most likely no")
+elif x==5:print("Most likely yes")
+elif x==6:print("Maybe")
+else:print("Outlook is not good")
