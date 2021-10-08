75836308b26b0bf1a8756b8be4653a2049b96394
Aiden Baker <33640860+SomeAspy@users.noreply.github.com>
2021-10-08 19:04:01 -0400

    WIP

--- college/Java-Suduko/src/App.java
+++ college/Java-Suduko/src/App.java
@@ -4,6 +4,24 @@ import java.util.ArrayList;
 public class App {
     public static ArrayList<Integer> rowA,rowB,rowC,rowD,rowE,rowF,rowG,rowH,rowI;
     public static void main(String[] args) throws Exception {
+        setup();
+        printBoard();
+        System.out.println("Welcome to Aiden's Sudoku solver!");
+        while(true){
+            System.out.print("You may:\n(Q)uit, (E)dit board, (P)rint board, or (S)olve\n");
+            Scanner input=new Scanner(System.in);
+            String menu=input.nextLine().toLowerCase();
+            switch(menu){
+                case "q":case "quit":input.close();System.exit(1);break;
+                case "e":case "edit":editBoard();break;
+                case "p":case "print":printBoard();break;
+                case "s":case "solve":solveBoard();break;
+                default:System.out.println("Oops! That's not an option!");
+            }
+        }
+    }
+    
+    static void setup(){
         //create values
         ArrayList<Integer>template=new ArrayList<>();
         for(int i=0; i<9; i++){
@@ -18,16 +36,23 @@ public class App {
         rowG=new ArrayList<>(template);
         rowH=new ArrayList<>(template);
         rowI=new ArrayList<>(template);
-        
-
-
-        printBoard();
     }
+    
     static void printBoard(){
         String board=rowA.toString()+rowB.toString()+rowC.toString()+rowD.toString()+rowE.toString()+rowF.toString()+rowG.toString()+rowH.toString()+rowI.toString();
         board=board.replace(", ","    ");
         board=board.replace("[","");
         board=board.replace("]","\n\n");
-        System.out.print(board);//NOSONAR
+        System.out.print(board);
+    }
+    
+    static void editBoard(){
+        String editRow;
+        int editindex;
+        
+    }
+
+    static void solveBoard(){
+
     }
 }
