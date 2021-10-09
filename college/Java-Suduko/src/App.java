1691a31567b383bb70b818af0213427e174f76bf
Aiden Baker <33640860+SomeAspy@users.noreply.github.com>
2021-10-09 09:53:39 -0400

    Enough for now

--- college/Java-Suduko/src/App.java
+++ college/Java-Suduko/src/App.java
@@ -1,58 +1,68 @@
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class App {
-    public static ArrayList<Integer> rowA,rowB,rowC,rowD,rowE,rowF,rowG,rowH,rowI;
+    public static int[][] board={
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0},
+    };
+    public static Scanner input=new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        setup();
         printBoard();
         System.out.println("Welcome to Aiden's Sudoku solver!");
         while(true){
-            System.out.print("You may:\n(Q)uit, (E)dit board, (P)rint board, or (S)olve\n");
-            Scanner input=new Scanner(System.in);
-            String menu=input.nextLine().toLowerCase();
-            switch(menu){
-                case "q":case "quit":input.close();System.exit(1);break;
-                case "e":case "edit":editBoard();break;
-                case "p":case "print":printBoard();break;
-                case "s":case "solve":solveBoard();break;
+            System.out.print("You may:\n(Q)uit\n(E)dit the board\n(P)rint the board\n(S)olve the board\n");
+            switch(input.nextLine().toLowerCase()){
+                case"q":case"quit":
+                    System.out.println("Are you sure you want to quit? You will lose everything!\n(Y)es\n(N)o");
+                    switch(input.nextLine().toLowerCase()){
+                        case"y":case"yes":input.close();System.exit(1);
+                        default:System.out.println("Canceled.");
+                    }
+                    break;
+                case"e":case"edit":editMenu();break;
+                case"p":case"print":printBoard();break;
+                case"s":case"solve":solveBoard();break;
                 default:System.out.println("Oops! That's not an option!");
             }
         }
     }
     
-    static void setup(){
-        //create values
-        ArrayList<Integer>template=new ArrayList<>();
-        for(int i=0; i<9; i++){
-            template.add(0);
-        }
-        rowA=new ArrayList<>(template);
-        rowB=new ArrayList<>(template);
-        rowC=new ArrayList<>(template);
-        rowD=new ArrayList<>(template);
-        rowE=new ArrayList<>(template);
-        rowF=new ArrayList<>(template);
-        rowG=new ArrayList<>(template);
-        rowH=new ArrayList<>(template);
-        rowI=new ArrayList<>(template);
-    }
     
     static void printBoard(){
-        String board=rowA.toString()+rowB.toString()+rowC.toString()+rowD.toString()+rowE.toString()+rowF.toString()+rowG.toString()+rowH.toString()+rowI.toString();
-        board=board.replace(", ","    ");
-        board=board.replace("[","");
-        board=board.replace("]","\n\n");
-        System.out.print(board);
+        System.out.println("  A   B   C   D   E   F   G   H   I\n  "+(Arrays.deepToString(board)).replace(", ", "   ").replace("[", "").replace("] ", "\n\n").replace("]]", "").replace("0", "X"));
     }
     
-    static void editBoard(){
-        String editRow;
-        int editindex;
+    static void editMenu(){
+        System.out.println("Entering editing mode...");
+        while(true){
+            System.out.print("EDITING MODE\nYou may:\n(R)eturn to main menu\n(C)heck board for errors\n(S)elect a block to edit\n");
+            switch(input.nextLine().toLowerCase()){
+                case"r":case"return":return;
+                case"c":case"check":checkBoard();break;
+                case"s":case"select":edit();break;
+                default:System.out.println("Oops! That's not an option!");
+            }
+        }
         
     }
 
     static void solveBoard(){
+
+    }
+
+    static void edit(){
+
+    }
+
+    static void checkBoard(){
 
     }
 }
