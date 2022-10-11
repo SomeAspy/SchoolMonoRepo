755ac772224fb358554f80bc6fb2c790ed3aec78
Aiden <amb@aspy.dev>
2022-10-11 10:21:52 -0400

    in class 10.11.22

---
+++ college/QCC2022/T1/inClass/inClass10.11.22/app.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2022 Aiden Baker
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string email, userName, domainName, TLD;
+    cin >> email;
+
+    int emailLength = email.length();
+
+    int atSymbol = email.find("@");
+    int dot = email.find(".");
+
+    userName = email.substr(0, atSymbol);
+    domainName = email.substr(atSymbol + 1, dot - atSymbol - 1);
+    TLD = email.substr(dot + 1, emailLength);
+    cout << "Username: " << userName << endl;
+    cout << "Domain: " << domainName << endl;
+    cout << "TLD: " << TLD << endl;
+    return 0;
+}
