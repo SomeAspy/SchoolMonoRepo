d901244a10af89d4862ce0fea46309391b5b12af
Aiden <amb@aspy.dev>
2022-10-18 10:36:14 -0400

    4.25

---
+++ college/QCC2022/T1/chapter4/4.25/app.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 Aiden Baker
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    while (num > 0)
+    {
+        cout << num % 2;
+        num /= 2;
+    }
+    cout << endl;
+
+    return 0;
+}
